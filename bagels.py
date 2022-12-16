669aa6ef3f6d415ddd6f25bbb207c014aa21ea0f
Niky Piky <naldorsc2@gmail.com>
2022-12-17 00:39:01 +0100

    adding intro text

---
+++ bagels.py
@@ -0,0 +1,12 @@
+import math
+
+print('I am thinking of a 3-digit number. Try to guess what it is.')
+print('Here are some clues:')
+print('When I say:    That means:')
+print('  Pico         One digit is correct but in the wrong position.')
+print('  Fermi        One digit is correct and in the right position.')
+print('  Bagels       No digit is correct.')
+print('I have thought up a number.')
+print(' You have 10 guesses to get it.')
+
+
